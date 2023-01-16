970e0fc2d2cf908a26728af9780b81e0f2991887
irfansami <irfasami00@gmail.com>
2023-01-16 17:17:28 +0700

    FIX ALL

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,13 @@ if(close){
     nav.classList.remove('active');
   })
 }
+
+let icon = document.getElementById("icon");
+icon.onclick = function(){
+  document.body.classList.toggle("darkmode");
+  if(document.body.classList.contains("darkmode")){
+    icon.src = "./src/images/home/moon.png";
+  }else{
+    icon.src = "./src/images/home/sun.png";
+  }
+}
